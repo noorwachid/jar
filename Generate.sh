e9b6c8fe90cbc910df53fd8be1476192374f64e2
Noor Wachid <wachid1818@gmail.com>
2020-10-21 08:38:45 +0700

    add generator

--- Generate.sh
+++ Generate.sh
@@ -1,2 +1,17 @@
 #!/bin/sh
 
+echo '' > index.html
+echo '<!DOCTYPE html>' >> index.html
+echo '<html>' >> index.html
+echo '<head>' >> index.html
+echo '    <meta charset="utf-8">' >> index.html
+echo '    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">' >> index.html
+echo '    <title>Matter</title>' >> index.html
+echo '    <link rel="stylesheet" href="style.css">' >> index.html
+echo '</head>' >> index.html
+echo '<body>' >> index.html
+
+find . -maxdepth 1 -type d -not -path '*.git*' -print0 | xargs -I {} -r0 echo '<a href="{}">{}</a>' >> index.html
+
+echo '</body>' >> index.html
+echo '</html>' >> index.html
